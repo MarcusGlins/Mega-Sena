0c749a403fecdb7597e33ccecef37f80db0ee015
Marcus Glins <marcusglins@gmail.com>
2022-07-27 01:45:46 -0300

    Modificação no métido exibe concursos

--- v0.0.1/Concursos.java
+++ v0.0.1/Concursos.java
@@ -1,15 +1,33 @@
 // Classe Concursos
 class Concursos {
   // Todos os concursos da Mega Sena
-  int[][] concursos = { { 4, 5,30,41,52}, { 9,37,39,41,43,49}, {10,11,29,30,36,47} };
+  int[][] concursos = { 
+    // Concursos de 1996
+    // Março: 3 concursos
+    { 4, 5,30,33,41,52}, { 9,37,39,41,43,49}, {10,11,29,30,36,47},
+    // Abril: 5 concursos
+    { 1, 5, 6,27,42,59}, { 1, 2, 6,16,19,46}, { 7,13,19,22,40,47},
+    { 3, 5,20,21,38,56}, { 4,17,37,38,47,53}
+  };
     
   // Método exibe concursos
   void exibeConcursos() {
+    int cont=1;
+
     for (int linha=0; linha < concursos.length; linha++) {
       for (int col=0; col < concursos[linha].length; col ++) {
-        System.out.printf("%d ", concursos[linha][col]);
+	System.out.printf("%2d ", concursos[linha][col]);
       }
-      System.out.println();
+      
+      cont++;
+
+      System.out.print(" | ");
+      
+      if (cont == 4) {
+        System.out.println();
+	cont = 1;
+      }
     }
+    System.out.println();
   }
 }
