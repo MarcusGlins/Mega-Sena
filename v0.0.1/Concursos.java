35f8564fcf4d8748f0541de9a5ffe8e2ae9c5858
Marcus Glins <marcusglins@gmail.com>
2022-08-09 23:57:27 -0300

    Alteração nas classes: Concursos (organização), MegaSena (organização)

--- v0.0.1/Concursos.java
+++ v0.0.1/Concursos.java
@@ -1,5 +1,5 @@
 // Classe Concursos
-class Concursos {
+public class Concursos {
   // Todos os concursos da Mega Sena
   int[][] concursos = { 
     // Concursos de 1996
@@ -7,7 +7,10 @@ class Concursos {
     { 4, 5,30,33,41,52}, { 9,37,39,41,43,49}, {10,11,29,30,36,47},
     // Abril: 5 concursos
     { 1, 5, 6,27,42,59}, { 1, 2, 6,16,19,46}, { 7,13,19,22,40,47},
-    { 3, 5,20,21,38,56}, { 4,17,37,38,47,53}
+    { 3, 5,20,21,38,56}, { 4,17,37,38,47,53},
+    // Maio: 4 concursos
+    { 8,43,54,55,56,60}, { 4,18,21,25,38,57}, {15,25,37,38,58,59},
+    { 4,16,19,20,27,43}
   };
     
   // Método exibe concursos
@@ -29,7 +32,7 @@ class Concursos {
       }
     }
     System.out.println();
-  }
+  } // fim do método exibeConcursos
 
   void numerosMaisFrequentes() {
     for (int linha=0; linha < this.concursos.length; linha++) {
@@ -37,6 +40,6 @@ class Concursos {
 	// Inserir código aqui	
       }
     }
-  }
-}
+  } // fim do método numerosMaisFrequentes
+} // fim da classe Concursos
 
--- v0.0.1/MegaSena.java
+++ v0.0.1/MegaSena.java
@@ -1,17 +1,13 @@
 // Classe Principal
-class MegaSena {
+public class MegaSena {
   public static void main(String[] args) {
     Concursos jogos = new Concursos();
 
     jogos.exibeConcursos();
     /*
-    int[][] jogos = {
+
     // Todos os concursos de 1996
-    // Março: 3 concursos
-      { 4, 5,30,33,41,52}, { 9,37,39,41,43,49}, {10,11,29,30,36,47},
-    // Abril: 5 concursos
-      { 1, 5, 6,27,42,59}, { 1, 2, 6,16,19,46}, { 7,13,19,22,40,47},
-      { 3, 5,20,21,38,56}, { 4,17,37,38,47,53}, 
+
     // Maio: 4 concursos
       { 8,43,54,55,56,60}, { 4,18,21,25,38,57}, {15,25,37,38,58,59}, 
       { 4,16,19,20,27,43},
@@ -91,5 +87,5 @@ class MegaSena {
 
     // Função que mostra os números mais frequentes
     */
-  }
-}
+  } // fim do método main
+} // fim da classe MegaSena
